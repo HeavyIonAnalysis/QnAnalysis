e6e259589d94e7ce64a30f99d84a9e222a0f25ac
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-09-15 22:21:17 +0300

    Update cmake_minimum required version

--- cmake_modules/QnTools.cmake
+++ cmake_modules/QnTools.cmake
@@ -6,7 +6,7 @@ if (NOT QnTools_FOUND)
     include(FetchContent)
     set(QnTools_BUILD_TESTS OFF)
     FetchContent_Declare(QnTools
-        GIT_REPOSITORY  git@github.com:eugene274/QnTools.git
+        GIT_REPOSITORY  https://github.com/eugene274/QnTools.git
         GIT_TAG         issue-33
         UPDATE_DISCONNECTED ${UPDATE_DISCONNECTED}
     )
