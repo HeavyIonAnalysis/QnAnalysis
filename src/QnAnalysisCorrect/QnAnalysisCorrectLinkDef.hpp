4b4f8ecbf3207612ce21a45ef27e3649897b45d8
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-09-16 12:31:34 +0300

    correct -> QnAnalysisCorrect

---
+++ src/QnAnalysisCorrect/QnAnalysisCorrectLinkDef.hpp
@@ -0,0 +1,10 @@
+#ifdef __MAKECINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace Qn;
+#pragma link C++ class Qn::Task + ;
+
+#endif
