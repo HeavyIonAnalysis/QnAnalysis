c3d733093c354125bd9f09e41090c4f0e8984065
Viktor <klochkov44@gmail.com>
2020-09-22 11:13:24 +0200

    add tags for externals

--- cmake_modules/QnTools.cmake
+++ cmake_modules/QnTools.cmake
@@ -5,7 +5,7 @@ find_package(QnTools QUIET)
 include(FetchContent)
 FetchContent_Declare(QnTools
         GIT_REPOSITORY https://github.com/HeavyIonAnalysis/QnTools.git
-        GIT_TAG master
+        GIT_TAG "v0.0.1"
         UPDATE_DISCONNECTED ${UPDATE_DISCONNECTED}
         )
 
