565bed83a29394cea7209a8a0f31860f5e8db454
Viktor <klochkov44@gmail.com>
2020-09-22 11:13:24 +0200

    add tags for externals

--- cmake_modules/AnalysisTree.cmake
+++ cmake_modules/AnalysisTree.cmake
@@ -7,7 +7,7 @@ set(AnalysisTree_BUILD_EXAMPLES OFF)
 include(FetchContent)
 FetchContent_Declare(AnalysisTree
         GIT_REPOSITORY  "https://github.com/HeavyIonAnalysis/AnalysisTree.git"
-        GIT_TAG         "master"
+        GIT_TAG         "v1.0.2"
         UPDATE_DISCONNECTED ${UPDATE_DISCONNECTED}
         )
 
