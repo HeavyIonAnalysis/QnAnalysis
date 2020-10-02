f251991dc9a44c394e68767681aa6577aa032fdb
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-10-02 16:07:54 +0300

    Starting QnAnalysisCorrelate from scratch

---
+++ src/QnAnalysisCorrelate/Config.hpp
@@ -0,0 +1,47 @@
+//
+// Created by eugene on 02/10/2020.
+//
+
+#ifndef QNANALYSIS_SRC_QNANALYSISCORRELATE_CONFIG_HPP
+#define QNANALYSIS_SRC_QNANALYSISCORRELATE_CONFIG_HPP
+
+#include "enum.h"
+#include <yaml-cpp/yaml.h>
+
+namespace Qn::Analysis::Correlate {
+
+
+
+struct Axis {
+
+};
+
+struct QVector {
+  std::string name;
+
+
+};
+
+/* arguments and one action */
+struct Correlation {
+
+};
+
+struct YAMLQuery {
+
+};
+
+/* list of arguments, list of actions to apply */
+struct CorrelationTask {
+
+
+};
+
+
+}
+
+namespace YAML {
+
+}
+
+#endif //QNANALYSIS_SRC_QNANALYSISCORRELATE_CONFIG_HPP
--- src/QnAnalysisCorrelate/Correlation.cpp
+++ src/QnAnalysisCorrelate/Correlation.cpp
@@ -11,38 +11,7 @@
 
 
 int main(int argc, char **argv) {
-  using namespace std;
-  using namespace boost::program_options;
 
-  CorrelationTask task;
-
-
-
-
-  try {
-    options_description desc("Common options");
-    desc.add_options()("help,h", "usage");
-
-    desc.add(task.GetBoostOptions());
-
-    variables_map vm;
-    store(parse_command_line(argc, argv, desc), vm);
-
-    if (vm.count("help")) {
-      std::cout << desc << std::endl;
-      return 0;
-    }
-
-    vm.notify();
-  }
-  catch (exception &e) {
-    cerr << e.what() << endl;
-    return 1;
-  }
-
-  task.Initialize();
-  task.Run();
-  task.Finalize();
 
   return 0;
 }
