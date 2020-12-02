c999c057243d2301079bc62e7c94f3209065029e
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-12-02 15:35:49 +0300

    Writing resource managers and things around it

--- src/QnAnalysisObservables/proto.cpp
+++ src/QnAnalysisObservables/proto.cpp
@@ -8,31 +8,225 @@
 
 #include <filesystem>
 #include <utility>
+#include <map>
+#include <tuple>
 
 #include <TFile.h>
 #include <TKey.h>
+#include <TDirectory.h>
 #include <TClass.h>
 
 #include <QnAnalysisTools/QnAnalysisTools.hpp>
 
+#include <DataContainer.hpp>
+#include <StatCollect.hpp>
+
+template<typename T>
+class Singleton {
+
+public:
+  virtual ~Singleton() = default;
+
+  static T &Instance() {
+    static T instance;
+    return instance;
+  }
+
+};
+
+template<typename T>
+class ResourceManager : public Singleton<ResourceManager<T>> {
+public:
+  typedef std::string KeyType;
+  typedef T ResourceType;
+  typedef T *PointerType;
+  typedef T &ReferenceType;
+
+  struct resource_exists_exception : public std::exception {
+    explicit resource_exists_exception(const char *ResourceName) : resource_name(ResourceName) {}
+    resource_exists_exception(const resource_exists_exception& other) = default;
+    const char* resource_name;
+  };
+
+  void Add(const KeyType &key, PointerType pointer) {
+    auto emplace_result = resources_.emplace(key, pointer);
+    if (!emplace_result.second) {
+      throw resource_exists_exception(key.c_str());
+    }
+  }
+  void Add(const KeyType& key, ReferenceType ref) {
+    Add(key, new ResourceType(ref));
+  }
+
+  bool Has(const KeyType& key) const {
+    auto it = resources_.find(key);
+    return it != resources_.end();
+  }
+
+  ReferenceType GetRef(const KeyType& key) const {
+    return *resources_.at(key);
+  }
+
+  PointerType GetPtr(const KeyType& key) const {
+    return resources_.at(key);
+  }
+
+  template<typename Function>
+  void ForEach(Function && f) {
+    for (auto [key,value] : resources_) {
+      f(key, GetRef(key));
+    }
+  }
+
+  void Print() {
+    std::cout << "Keys: " << std::endl;
+    for (auto &element : resources_) {
+      std::cout << "\t" << element.first << std::endl;
+    }
+  }
+
+private:
+
+  std::map<KeyType, PointerType> resources_;
+};
+
+namespace Details {
+
+template <typename T>
+struct FunctionTraits
+{
+  template <typename R, typename ... As>
+  static ::std::tuple<std::decay_t<As>...> pro_args (std::function<R(As...)>);
+
+  using ArgumentsTuple = decltype(pro_args(std::function{std::declval<T>()}));
+};
+
+std::vector<std::string> FindTDirectory(const TDirectory &dir, const std::string &cwd = "") {
+  std::vector<std::string> result;
+
+  for (auto o : *dir.GetListOfKeys()) {
+    auto key_ptr = static_cast<TKey*>(o);
+    if(TClass::GetClass(key_ptr->GetClassName())->InheritsFrom(TDirectory::Class())) {
+      auto nested_dir = static_cast<TDirectory*>(key_ptr->ReadObj());
+      auto nested_contents = FindTDirectory(*nested_dir, cwd + "/" + nested_dir->GetName());
+      std::move(std::begin(nested_contents), std::end(nested_contents), std::back_inserter(result));
+    } else {
+      result.emplace_back(cwd + "/" + key_ptr->GetName());
+    }
+  }
+  return result;
+}
+
+template<typename Tuple, std::size_t IArg>
+bool SetArgI(std::vector<std::string> arg_names, Tuple &tuple) {
+  using ArgT = std::tuple_element_t<IArg, Tuple>;
+  auto &manager = ResourceManager<ArgT>::Instance();
+  if (manager.Has(arg_names[IArg])) {
+    std::get<IArg>(tuple) = manager.GetRef(arg_names[IArg]);
+    return true;
+  }
+  return false;
+}
+
+template<typename Tuple, std::size_t ... IArg>
+bool SetArgTupleImpl(std::vector<std::string> arg_names, Tuple &tuple, std::index_sequence<IArg...>) {
+  std::vector<bool> result {SetArgI<Tuple,IArg>(arg_names, tuple)...};
+  return std::all_of(result.begin(), result.end(), [] (bool is_ok) { return is_ok; });
+}
+
+template<typename ... Args>
+bool SetArgTuple(std::vector<std::string> arg_names, ::std::tuple<Args...> &tuple) {
+  return SetArgTupleImpl(arg_names, tuple, std::make_index_sequence<sizeof...(Args)>());
+}
+
+
+}
+
+template<typename T>
+void AddResource(std::string name, T&& ref) {
+  ResourceManager<T>::Instance().Add(name, ref);
+}
+
+template<typename T>
+void AddResource(std::string name, T* ref) {
+  ResourceManager<T>::Instance().Add(name, ref);
+}
+
+template <typename T>
+void LoadFile(const char *file_name) {
+  TFile f(file_name, "READ");
+
+  for (const auto& path : Details::FindTDirectory(f)) {
+    auto ptr = f.Get<T>(path.c_str());
+    if (ptr) {
+      std::cout << "Adding path '" << path << "'" << std::endl;
+      AddResource(path, ptr);
+    }
+  }
+}
+
+template<typename T>
+void ExportToROOT(const char *filename) {
+  TFile f(filename, "RECREATE");
+  ResourceManager<T>::Instance().ForEach([&f] (const std::string& name, const T& value) {
+
+  });
+}
+
+
+template <typename Function>
+void Define(const std::string& name, Function && function, std::vector<std::string> arg_names) {
+  using ArgsTuple = typename Details::FunctionTraits<Function>::ArgumentsTuple;
+  ArgsTuple args;
+  if (Details::SetArgTuple(arg_names, args)) {
+    AddResource(name, std::apply(function, args));
+  } else {
+    Warning(__func__, "Some arguments for '%s' are missing", name.c_str());
+  }
+}
+
+
+Qn::DataContainerStatCalculate Resolution3S(const Qn::DataContainerStatCalculate &nom1,
+                                            const Qn::DataContainerStatCalculate &nom2,
+                                            const Qn::DataContainerStatCalculate &denom) {
+  auto nom = nom1 * nom2;
+  nom = 2 * nom;
+  return Qn::Sqrt(nom / denom);
+}
 
 
 int main() {
+  using std::string;
+  using std::get;
 
   TFile f("correlation.root", "READ");
+  LoadFile<Qn::DataContainerStatCollect>(f.GetName());
 
-  /*
-   * Note 1: Two types of objects:
-   *  DataContainer<StatsCollect>:  for Rebins,Merges,etc
-   *  DataContainer<StatsCalculate>: Arithmetics
-   */
+  ResourceManager<Qn::DataContainerStatCollect>::Instance().ForEach([] (const std::string &name, Qn::DataContainerStatCollect collect) {
+    AddResource(name, Qn::DataContainerStatCalculate(collect));
+  });
 
-  using namespace Qn::Analysis::Tools;
+  ResourceManager<Qn::DataContainerStatCalculate>::Instance().Print();
 
-  for (auto c : Permutation(std::vector<int>{1,2,2,3})) {
-    std::cout << c[0] << " " << c[1] << " " << c[2] << " " << c[3] << std::endl;
+  std::list<std::tuple<string, string, string>> args_list = {
+      {"psd1", "psd2", "psd3"},
+      {"psd2", "psd1", "psd3"},
+      {"psd3", "psd1", "psd2"},
+  };
+
+  const string RECENTERED = "_RECENTERED";
+  const string x1x1 = "x1x1";
+  const string y1y1 = "y1y1";
+
+  for (auto &args : args_list) {
+    auto arg1_name = "/QQ/" + get<0>(args) + RECENTERED + "." + get<1>(args) + RECENTERED + "." + y1y1;
+    auto arg2_name = "/QQ/" + get<0>(args) + RECENTERED + "." + get<2>(args) + RECENTERED + "." + y1y1;
+    auto arg3_name = "/QQ/" + get<1>(args) + RECENTERED + "." + get<2>(args) + RECENTERED + "." + y1y1;
+    auto resolution = "/resolution/RES_" + get<0>(args) + "_" + y1y1;
+    Define(resolution, Resolution3S, {arg1_name, arg2_name, arg3_name});
   }
 
+  ResourceManager<Qn::DataContainerStatCalculate>::Instance().Print();
 
 
 
