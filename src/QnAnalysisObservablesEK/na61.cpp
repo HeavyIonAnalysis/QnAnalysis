ea86edc198c682f63314f13f11f9968ff7ec97d1
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2021-03-26 01:20:22 +0100

    NA61 Setup: analysis of PSD resolutions

--- src/QnAnalysisObservablesEK/na61.cpp
+++ src/QnAnalysisObservablesEK/na61.cpp
@@ -183,7 +183,8 @@ int main() {
   });
 
   {
-    const std::string uQ_reco_expr("^/calc/uQ/(pion_neg|protons)_(pt|y)_set_(\\w+)_(PLAIN|RECENTERED|TWIST|RESCALED).*$");
+    const std::string
+        uQ_reco_expr("^/calc/uQ/(pion_neg|protons)_(pt|y)_set_(\\w+)_(PLAIN|RECENTERED|TWIST|RESCALED).*$");
     gResourceManager.ForEach([&uQ_reco_expr](const StringKey &key, ResourceManager::Resource &r) {
       auto particle = KEY.MatchGroup(1, uQ_reco_expr)(r);
       auto axis = KEY.MatchGroup(2, uQ_reco_expr)(r);
@@ -350,72 +351,67 @@ int main() {
   {
     /***************** RESOLUTION 4-sub ******************/
 
-    /*
-     * Prepare different references from TPC to use in 4-subevents method
-     */
-    gResourceManager.ForEach([](VectorKey key, const ResourceManager::Resource &r) {
-      using Resource = ResourceManager::Resource;
 
-      auto calc = r.As<DTCalc>();
+    auto build_4sub_resolution = [](
+        const std::string &meta_key,
+        const std::string& tpc_ref_base,
+        const std::vector<std::string>& psd_subs_base) {
+      const std::vector<std::string> components = {"x1x1", "y1y1"};
+      const std::string tpc_ref_cstep = "RESCALED";
+      const std::string psd_subs_cstep = "RECENTERED";
+      for (const auto &component : components) {
+        const auto subA = psd_subs_base[0];
+        const auto subB = psd_subs_base[1];
+        const auto subC = psd_subs_base[2];
+        const auto resolution_component = component == "x1x1" ? "X" : "Y";
+        auto tpc_ref_subA = gResourceManager.GetMatching(KEY == (Format("/calc/uQ/%1%_%2%.%3%_%4%.%5%") % tpc_ref_base
+            % tpc_ref_cstep % subA % psd_subs_cstep % component).str()).at(0);
+        auto tpc_ref_subB = gResourceManager.GetMatching(KEY == (Format("/calc/uQ/%1%_%2%.%3%_%4%.%5%") % tpc_ref_base
+            % tpc_ref_cstep % subB % psd_subs_cstep % component).str()).at(0);
+        auto tpc_ref_subC = gResourceManager.GetMatching(KEY == (Format("/calc/uQ/%1%_%2%.%3%_%4%.%5%") % tpc_ref_base
+            % tpc_ref_cstep % subC % psd_subs_cstep % component).str()).at(0);
+        auto subA_subC = gResourceManager.GetMatching(KEY == (Format("/calc/QQ/%1%_%2%.%3%_%4%.%5%") % subA
+            % psd_subs_cstep % subC % psd_subs_cstep % component).str()).at(0);
 
-      auto meta = r.meta;
-      meta.put("type", "4sub_tpc_ref");
+        auto apply_abs = [] (const std::string& key) {
+          auto &calc = gResourceManager.Get(key, ResourceManager::ResTag<DTCalc>());
+          calc = Qn::Sqrt(Qn::Pow(calc, 2.));
+        };
+        apply_abs(tpc_ref_subA);
+        apply_abs(tpc_ref_subB);
+        apply_abs(tpc_ref_subC);
 
-      auto reference_name = "reference_" + META["arg1.name"](r);
-      auto component = META["component"](r);
+        ::Predicates::MetaTemplateGenerator resolution_key_generator(
+            "/resolution/{{resolution.meta_key}}/RES_{{resolution.ref}}_{{resolution.component}}");
 
-      {
-        auto selected = calc.Select(Qn::AxisD("RecParticles_y_cm", 1, 0.8, 1.2));
-        VectorKey new_key = {"resolution", "4sub_protons_08_12", "4sub", reference_name, component};
-        meta.put("4sub_meta_key", "4sub_protons_08_12");
-        AddResource(new_key, Resource(selected, meta));
-      }
-      {
-        auto selected = calc.Select(Qn::AxisD("RecParticles_y_cm", 1, 0.4, 0.8));
-        VectorKey new_key = {"resolution", "4sub_protons_04_08", "4sub", reference_name, component};
-        meta.put("4sub_meta_key", "4sub_protons_04_08");
-        AddResource(new_key, Resource(selected, meta));
-      }
-      {
-        auto selected = calc.Select(Qn::AxisD("RecParticles_y_cm", 1, 0.0, 0.4));
-        VectorKey new_key = {"resolution", "4sub_protons_00_04", "4sub", reference_name, component};
-        meta.put("4sub_meta_key", "4sub_protons_00_04");
-        AddResource(new_key, Resource(selected, meta));
-      }
-    }, KEY.Matches("/calc/uQ/protons_y_standard_RESCALED\\.(psd[1-3])_RECENTERED\\.(x1x1|y1y1)$"));
+        Meta meta_r_rtpc;
+        meta_r_rtpc.put("type", "resolution_4sub_aux");
+        meta_r_rtpc.put("resolution.meta_key", meta_key);
+        meta_r_rtpc.put("resolution.ref", "TPC");
+        meta_r_rtpc.put("resolution.component", resolution_component);
+        auto r_tpc_key = Define(resolution_key_generator,
+                                Methods::Resolution3S,
+                                {tpc_ref_subA, tpc_ref_subC, subA_subC},
+                                meta_r_rtpc)->name;
 
-    for (auto &&[resolution_meta_key, component] : Tools::Combination(
-        gResourceManager.SelectUniq(META["4sub_meta_key"], META["type"] == "4sub_tpc_ref"),
-        std::vector<std::string>{"x1x1", "y1y1"})) {
+        auto make_meta = [meta_key, resolution_component](const std::string &ref) {
+          Meta meta;
+          meta.put("resolution.meta_key", meta_key);
+          meta.put("resolution.ref", ref);
+          meta.put("resolution.ref_alias", ref);
+          meta.put("resolution.component", resolution_component);
+          return meta;
+        };
 
-      auto key_generator =
-          "/resolution/" + META["resolution.meta_key"] +
-              "/RES_" + META["resolution.ref"] + "_" + META["resolution.component"];
+        Define(resolution_key_generator, Methods::Resolution4S, {subA_subC, r_tpc_key, tpc_ref_subC}, make_meta(subA));
+        Define(resolution_key_generator, Methods::Resolution4S_1, {tpc_ref_subB, r_tpc_key}, make_meta(subB));
+        Define(resolution_key_generator, Methods::Resolution4S, {subA_subC, r_tpc_key, tpc_ref_subA}, make_meta(subC));
+      }// components
+    };
 
-      Meta meta;
-      meta.put("resolution.meta_key", resolution_meta_key);
-      meta.put("resolution.component", component);
-
-      meta.put("resolution.ref", "TPC");
-      Define(key_generator, Methods::Resolution3S,
-             {"/resolution/" + resolution_meta_key + "/protons_y_RESCALED.psd1_RECENTERED." + component,
-              "/resolution/" + resolution_meta_key + "/protons_y_RESCALED.psd3_RECENTERED." + component,
-              "/calc/QQ/psd1_RECENTERED.psd3_RECENTERED." + component}, meta);
-      meta.put("resolution.ref", "psd1");
-      Define(key_generator, Methods::Resolution4S,
-             {"/calc/QQ/psd1_RECENTERED.psd3_RECENTERED." + component,
-              "/resolution/" + resolution_meta_key + "/RES_TPC_" + component,
-              "/resolution/" + resolution_meta_key + "/protons_y_RESCALED.psd3_RECENTERED." + component}, meta);
-      meta.put("resolution.ref", "psd2");
-      Define(key_generator, Methods::Resolution4S_1,
-             {"/resolution/" + resolution_meta_key + "/protons_y_RESCALED.psd2_RECENTERED." + component,
-              "/resolution/" + resolution_meta_key + "/RES_TPC_" + component}, meta);
-      meta.put("resolution.ref", "psd3");
-      Define(key_generator, Methods::Resolution4S,
-             {"/calc/QQ/psd1_RECENTERED.psd3_RECENTERED." + component,
-              "/resolution/" + resolution_meta_key + "/RES_TPC_" + component,
-              "/resolution/" + resolution_meta_key + "/protons_y_RESCALED.psd1_RECENTERED." + component}, meta);
-    }
+    build_4sub_resolution("4sub_preliminary", "4sub_ref_preliminary", {"psd1", "psd2", "psd3"});
+    build_4sub_resolution("4sub_opt1", "4sub_ref_opt1", {"psd1", "psd2", "psd3"});
+    build_4sub_resolution("4sub_opt2", "4sub_ref_opt2", {"psd1", "psd2", "psd3"});
 
   }
 
@@ -727,9 +723,12 @@ int main() {
             [&save_dir](auto feature, const std::vector<ResourceManager::ResourcePtr> &resolution_ptrs) {
               const std::map<std::string, int> colors = {
                   {"psd_mc", kBlack},
-                  {"psd90_mc", kOrange},
+                  {"psd90_mc", kOrange + 1},
                   {"3sub_standard", kRed},
                   {"3sub_psd90", kGreen + 2},
+                  {"4sub_preliminary", kBlue},
+                  {"4sub_opt1", kMagenta},
+                  {"4sub_opt2", kCyan+1},
               };
 
               TMultiGraph mg;
@@ -738,6 +737,7 @@ int main() {
                 auto resolution_graph = Qn::ToTGraph(resolution_calc);
                 resolution_graph->SetLineColor(colors.at(META["resolution.meta_key"](*r)));
                 resolution_graph->SetMarkerColor(colors.at(META["resolution.meta_key"](*r)));
+                resolution_graph->SetLineWidth(2.);
                 resolution_graph->SetTitle(META["resolution.meta_key"](*r).c_str());
                 mg.Add(resolution_graph, "lp");
               }
