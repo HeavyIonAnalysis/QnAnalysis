885bc2f142ca8ccc3784eebe743011df2046fac2
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2021-12-28 14:39:49 +0300

    Tensor operations

--- src/QnAnalysisObservablesEK/Correlation/Correlation.hpp
+++ src/QnAnalysisObservablesEK/Correlation/Correlation.hpp
@@ -10,11 +10,197 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <tuple>
 
 #include <QnTools/DataContainer.hpp>
 #include <QnTools/StatCalculate.hpp>
 
 namespace C4 {
+
+namespace TensorOps {
+
+typedef std::map<std::string, size_t> TensorAxes;
+typedef std::map<std::string, size_t> TensorIndex;
+typedef std::size_t TensorLinearIndex;
+
+template<typename T>
+struct Tensor {
+  typedef T value_type;
+
+  typedef std::function<value_type(const TensorIndex &)> factory_function_type;
+
+  Tensor(TensorAxes axes, factory_function_type factory_function)
+      : axes_(std::move(axes)), factory_function_(factory_function) {
+    for (auto && [axis_name, _] : axes_) {
+      ax_names_.emplace_back(axis_name);
+    }
+  }
+
+
+  TensorIndex getIndex(TensorLinearIndex idx) const {
+    TensorIndex result;
+    for (auto && axis_name : ax_names_) {
+      auto axis_size = axes_.at(axis_name);
+      auto axis_id = idx % axis_size;
+      result.template emplace(axis_name, axis_id);
+      idx /= axis_size;
+    }
+    return result;
+  }
+  TensorLinearIndex getLinearIndex(const TensorIndex &idx) const {
+    checkIndex(idx);
+    TensorLinearIndex result = 0ul;
+
+    std::vector<std::string> r_ax_names(ax_names_.rbegin(), ax_names_.rend());
+    for (auto && axis_name : r_ax_names) {
+      auto ax_idx = idx.at(axis_name);
+      result *= axes_.at(axis_name);
+      result += ax_idx;
+    }
+    return result;
+  }
+  std::vector<std::string> getAxNames() const {
+    return ax_names_;
+  }
+
+  value_type at(const TensorIndex &idx) const {
+    checkIndex(idx);
+    return factory_function_(idx);
+  }
+
+  value_type at(TensorLinearIndex idx) const {
+    return at(getIndex(idx));
+  }
+
+  TensorLinearIndex size() const {
+    TensorLinearIndex result = 1;
+    for (auto &&[_, axis_size] : axes_) {
+      result *= axis_size;
+    }
+    return result;
+  }
+
+  template<typename OT, typename BinaryFunction>
+  auto
+  applyBinary(BinaryFunction &&binary_function, const OT &other_arg) const {
+    auto new_factory_function = [
+        lhs = factory_function_,
+        rhs = other_arg,
+        outer = binary_function
+    ](const TensorIndex &index) {
+      return outer(lhs(index), rhs);
+    };
+    return Tensor{axes_, new_factory_function};
+  }
+
+  template<typename OT, typename BinaryFunction>
+  auto
+  applyBinary(BinaryFunction &&binary_function, const Tensor<OT> &other_tensor) const {
+    auto new_axis = mergeAxes(axes_, other_tensor.axes_);
+    auto new_factory_function = [
+        lhs = factory_function_,
+        rhs = other_tensor.factory_function_,
+        outer = std::forward<BinaryFunction>(binary_function)](const TensorIndex &index) {
+      return outer(lhs(index), rhs(index));
+    };
+    return Tensor{new_axis, new_factory_function};
+  }
+
+  template<typename OT>
+  auto operator * (const OT& other) const {
+    return applyBinary([] (auto && l, auto && r) { return l*r; }, other);
+  }
+  template<typename OT>
+  auto operator / (const OT& other) const {
+    return applyBinary([] (auto && l, auto && r) { return l / r; }, other);
+  }
+
+  template<typename UnaryFunction>
+  auto applyUnary(UnaryFunction &&unary_function) const {
+    auto new_factory_function = [
+        inner = factory_function_,
+        outer = unary_function](const TensorIndex &ind) {
+      return outer(inner(ind));
+    };
+    return Tensor(axes_, factory_function_);
+  }
+
+  void checkIndex(const TensorIndex &ind) const {
+    for (auto &&[ax_name, size] : axes_) {
+      if (ind.at(ax_name) >= size) {
+        throw std::out_of_range("out of range for '" + ax_name + "'");
+      }
+    }
+  }
+
+  static inline
+  TensorAxes
+  mergeAxes(const TensorAxes &lhs, const TensorAxes &rhs) {
+    TensorAxes result = lhs;
+    for (auto &&[axis_name, axis_size] : rhs) {
+      auto emplace_result = result.emplace(axis_name, axis_size);
+      if (!emplace_result.second) { /* common axis */
+        if (emplace_result.first->second != axis_size) {
+          throw std::runtime_error("Common axis '" + axis_name + "' must have the same size");
+        }
+      }
+    }
+    return result;
+  }
+
+  std::vector<std::string> ax_names_;
+  TensorAxes axes_;
+  factory_function_type factory_function_;
+};
+
+
+template<typename Function> Tensor(TensorAxes, Function &&) ->
+Tensor<std::decay_t<std::invoke_result_t<Function, const TensorIndex &>>>;
+
+template<typename Left, typename Right>
+auto operator * (const Left& l, const Tensor<Right> & r) {
+  return r.operator*(l);
+}
+
+template<typename T>
+struct Enumeration {
+
+  template<typename Container>
+  Enumeration(std::string name, Container &&c) : name_(std::move(name)) {
+    TensorLinearIndex value_index = 0ul;
+    for (T &v : c) {
+      index_.emplace(value_index, v);
+      ++value_index;
+    }
+  }
+
+  T operator()(const TensorIndex &ind) const {
+    return index_.at(ind.at(name_));
+  }
+
+  explicit operator Tensor<T>() const {
+    return Tensor({{name_, std::size(index_)}}, *this);
+  }
+
+  TensorLinearIndex size() const {
+    return index_.size();
+  }
+
+  std::string name_;
+  std::map<TensorLinearIndex, T> index_;
+};
+
+template<typename Container> Enumeration(std::string name,
+                                         Container &&c) -> Enumeration<typename Container::value_type>;
+
+template<typename T>
+auto enumerate(std::string name, std::initializer_list<T> args) {
+  return Enumeration(name, std::vector<T>(args));
+}
+
+} // namespace TensorOps
+
+namespace Correlations {
 
 enum class EComponent {
   X, Y
@@ -50,8 +236,8 @@ struct Correlation {
 
   std::string nameInFile() const {
     static const std::map<EComponent, std::string> map_component{
-      {EComponent::X, "x"},
-      {EComponent::Y, "y"}
+        {EComponent::X, "x"},
+        {EComponent::Y, "y"}
     };
 
     std::stringstream stream;
@@ -103,14 +289,13 @@ struct UnaryOp {
   }
 
   template<typename Function1>
-  auto apply(Function1 && f) {
+  auto apply(Function1 &&f) {
     return UnaryOp(*this, std::forward<Function1>(f));
   }
 
   Arg arg_;
   Function function_;
 };
-
 
 template<typename T>
 struct Value {
@@ -139,8 +324,6 @@ auto operator/(const L &cl, const R &cr) {
   });
 }
 
-
-
 template<typename V>
 auto sqrt(const V &v) {
   return UnaryOp(v, [](const auto &v) {
@@ -156,6 +339,8 @@ QVec q(std::string name, unsigned int harmonic, EComponent component) {
 template<typename ... QVs>
 Correlation C(QVs ... qvs) {
   return Correlation({qvs...});
+}
+
 }
 
 }
--- src/QnAnalysisObservablesEK/Correlation/Correlation.test.cpp
+++ src/QnAnalysisObservablesEK/Correlation/Correlation.test.cpp
@@ -6,10 +6,10 @@
 
 #include "Correlation.hpp"
 
-using namespace C4;
-
 
 TEST(Correlation, Basics) {
+  using namespace ::C4::Correlations;
+
   auto q1 = q("psd1_RECENTERED", 1, EComponent::X);
   auto q2 = q("psd2_RECENTERED", 1, EComponent::X);
   auto q3 = q("psd3_RECENTERED", 1, EComponent::X);
@@ -19,5 +19,46 @@ TEST(Correlation, Basics) {
   r1.value().Print();
 }
 
+TEST(Tensor, Basics) {
+  using namespace C4::TensorOps;
 
+  auto t1 = Tensor<std::string>(Enumeration("ref",std::vector<std::string>({"psd1", "psd2", "psd3"})));
+  EXPECT_EQ(t1.size(), 3);
+  EXPECT_NO_THROW(t1.at({{"ref", 2}}));
+  EXPECT_THROW(t1.at({{"ref1", 1}}), std::out_of_range);
+  EXPECT_THROW(t1.at({{"ref", 3}}), std::out_of_range);
+}
 
+TEST(Tensor, Indexing) {
+  using namespace C4::TensorOps;
+  auto t1 = Tensor<double>(enumerate("ref", {1.,2.,3.}));
+  auto t2 = Tensor<double>(enumerate("component", {10.,20.,30.}));
+  auto t3 = Tensor<double>(enumerate("qwerty", {100.,200.,300.}));
+
+  for (TensorLinearIndex li = 0ul; li < t1.size(); ++li) {
+    auto ind = t1.getIndex(li);
+    auto il = t1.getLinearIndex(ind);
+    EXPECT_EQ(li, il);
+  }
+  auto result = t1*t2*t3;
+  for (TensorLinearIndex li = 0ul; li < result.size(); ++li) {
+    auto ind = result.getIndex(li);
+    auto il = result.getLinearIndex(ind);
+    EXPECT_EQ(li, il);
+  }
+
+}
+
+TEST(Tensor, BinaryOps) {
+  using namespace C4::TensorOps;
+  auto t1 = Tensor<double>(enumerate("ref", {1.,2.,3.}));
+  auto t2 = Tensor<double>(enumerate("component", {10.,20.}));
+  double t3 = 11;
+  auto result = t1 * t2 / t3;
+  EXPECT_EQ(result.size(), t1.size()*t2.size());
+  EXPECT_EQ(result.getAxNames(), std::vector<std::string>({"component", "ref"}));
+
+  auto result1 = t3 * t1 / t2;
+  EXPECT_EQ(result1.size(), t1.size()*t2.size());
+  EXPECT_EQ(result1.getAxNames(), std::vector<std::string>({"component", "ref"}));
+}
